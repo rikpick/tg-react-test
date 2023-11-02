0b2c6c663688243aad9f51daa28c5e7f3917a72f
Rik Pick~ <g29043066@gmail.com>
2023-11-02 22:09:32 +0200

    user

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -10,7 +10,7 @@ const Header = (props) => {
         <div className={'header'}>
          <Button onClick={onClose}>Close</Button>
          <span className={'username'}>
-            user?.username
+            {user?.username}
          </span>
         </div>
     );
