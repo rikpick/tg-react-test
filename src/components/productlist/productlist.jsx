b2ed1f8bb6f65a7889eb5fd661c67dec7bab5e9c
Rik Pick~ <g29043066@gmail.com>
2023-11-05 17:34:11 +0200

    6

--- src/components/productlist/productlist.jsx
+++ src/components/productlist/productlist.jsx
@@ -54,7 +54,7 @@ const ProductList = () => {
                     className={'item'}
                 />
             ))}
-            {addedItems}
+            `${addedItems}`
         </div>
     );
 };
