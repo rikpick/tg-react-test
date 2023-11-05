38ae32df33095f213a6c182b9962ed120b56999b
Rik Pick~ <g29043066@gmail.com>
2023-11-05 18:56:50 +0200

    11

--- src/components/productlist/productlist.jsx
+++ src/components/productlist/productlist.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './productlist.css';
 import { useTelegram } from "../../hooks/useTelegram";
 import ProductItem from "../productitem/productitem";
+import React, { useEffect, useCallback } from "react";
 
 const products = [
     {id: '1', title: '1g', price: 699, description: '1 gr'},
@@ -20,7 +21,31 @@ const getTotalPrice = (items = []) => {
 const ProductList = () => { 
     const [addedItems, setAddedItems] = useState([]);
 
-    const {tg} = useTelegram();
+    const {tg, queryId} = useTelegram();
+
+    const onSendData = useCallback(() => {
+        const data = {
+           products: addedItems,
+           totalPrice: getTotalPrice(addedItems),
+           queryId,
+        }
+        fetch('http//localhost:8000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+
+    }, [])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+
+    }, [onSendData])
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -19,6 +19,7 @@ export function useTelegram() {
         onClose,
         tg,
         user: tg.initDataUnsafe?.user,
+        queryId: tg.initDataUnsafe?.query_id,
      }
 
 }
