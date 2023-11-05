fc7119ee26867289711d56d15aba078c682f637d
Rik Pick~ <g29043066@gmail.com>
2023-11-05 16:33:13 +0200

    send data 2

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -17,7 +17,7 @@ const Form = () => {
         }
         tg.sendData(JSON.stringify(data));
 
-    })
+    }, [name, tel, dost])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
@@ -25,7 +25,7 @@ const Form = () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
 
-    }, [])
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
