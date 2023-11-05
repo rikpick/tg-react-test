60b8715852dd3050a92a49da2cec66070f4d19bc
Rik Pick~ <g29043066@gmail.com>
2023-11-05 18:02:14 +0200

    9

--- src/components/productlist/productlist.jsx
+++ src/components/productlist/productlist.jsx
@@ -11,11 +11,10 @@ const products = [
     {id: '5', title: '5g', price: '2999', description: '5 gr'},
 ]
 
-const getTotalPrice = (items) => {
+const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
         return acc += item.price
     }, 0)
-
 }
 
 const ProductList = () => { 
