8471f33ae9101836f72566e5fd403fc9c1be93e9
Rik Pick~ <g29043066@gmail.com>
2023-11-07 11:22:32 +0200

    21

--- src/components/productlist/productlist.jsx
+++ src/components/productlist/productlist.jsx
@@ -62,7 +62,7 @@ const ProductList = () => {
         } else {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(newItems)}`
+                text: `Перейти в корзину ₴${getTotalPrice(newItems)}`
             });
         }
 
