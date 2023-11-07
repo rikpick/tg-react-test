364a231f611d33cc87e4b746c689e3638b6a2225
Rik Pick~ <g29043066@gmail.com>
2023-11-07 10:49:34 +0200

    12

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -3,7 +3,7 @@ import './button.css';
 
 const Button = (props) => {
     return (
-        <button {...props} className={'button' + props.className}/>
+        <button {...props} className={'button ' + props.className}/>
     );
 };
 
