3df17aa3a0fe1db592a27b23fc950dcf42eb2496
Rik Pick~ <g29043066@gmail.com>
2023-12-02 15:58:38 +0200

    mb

--- src/components/productlist/productlist.jsx
+++ src/components/productlist/productlist.jsx
@@ -16,7 +16,7 @@ const products = [
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
         return acc += item.price
-    }, 0)
+    }, klad === 'nova-pochta' ? 50 : 0)
 }
 
 const {user, onClose} = useTelegram()
