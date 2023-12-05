618a8e9a8587039576be7b3564c892bed442b1cd
Rik Pick~ <g29043066@gmail.com>
2023-12-05 17:00:15 +0200

    clever

--- src/components/productlist/productlist.jsx
+++ src/components/productlist/productlist.jsx
@@ -41,15 +41,15 @@ const ProductList = () => {
             if (addedItems.length === 0) {
                 tg.MainButton.setParams({
                     color: "#FFB000",
+                    textColor: "#242424",
                     text: `Добавьте товар в Корзину`,
-                    textColor: "#242424"
                 });
             } else {
                 tg.MainButton.show();
                 tg.MainButton.setParams({
                     color: "#FFB000",
+                    textColor: "#242424",
                     text: `Оформить заказ  ₴${cartPrice}`,
-                    textColor: "#242424"
                 })
             }
 
