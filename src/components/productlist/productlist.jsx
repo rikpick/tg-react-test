5dd96b44d3068609e78694e8dcd9a497e8e24811
Rik Pick~ <g29043066@gmail.com>
2023-12-24 15:56:43 +0200

    44

--- src/components/productlist/productlist.jsx
+++ src/components/productlist/productlist.jsx
@@ -70,7 +70,7 @@ const ProductList = () => {
            sort: sort,
            klad: klad
         }
-        fetch('https://localhost:8000/web-data', {
+        fetch('https://gn2vn7k6-8000.euw.devtunnels.ms/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
