5a37bc04c1d92bf534ba23ccfd67dfa2bd5575e1
Rik Pick~ <g29043066@gmail.com>
2024-03-05 17:43:06 +0200

    123

--- src/components/productlist/productlist.jsx
+++ src/components/productlist/productlist.jsx
@@ -25,7 +25,7 @@ const {user, onClose} = useTelegram()
 
 const ProductList = () => { 
     const [addedItems, setAddedItems] = useState([]);
-    const [dost, setDost] = useState('dnipro')
+    const [dost, setDost] = useState('ukr')
     const [pay, setPay] = useState('card')
     const [sort, setSort] = useState('superskunk')
     const [klad, setKlad] = useState('nova-pochta')
@@ -106,12 +106,12 @@ const ProductList = () => {
 
     const onChangeDost = (e) => {
         setDost(e.target.value)
-        if (e.target.value === 'ukr') {
+        /*if (e.target.value === 'ukr') {
             document.getElementById('visible').disabled = true;
             setKlad("nova-pochta")
         } else {
             document.getElementById('visible').disabled= false;
-        }
+        }*/
     }
 
     const onChangePay = (e) => {
@@ -147,14 +147,12 @@ const ProductList = () => {
 
         <select value={sort} onChange={onChangeSort} className={'select'}>
             <option value={'superskunk'}>Super Skunk</option>
-            <option value={'brucebenner'}>Bruce Benner</option>
-            <option value={'bigdewil'}>Big Dewil</option>
+            <option value={'ak47'}>AK-47</option>
         </select>
 
         <h3>Город</h3>
         
         <select value={dost} onChange={onChangeDost} className={'select'}>
-            <option value={'dnipro'}>Днепр</option>
             <option value={'ukr'}>Вся Украина</option>
         </select>
 
@@ -162,7 +160,6 @@ const ProductList = () => {
 
         <select value={klad} onChange={onChangeKlad} className={'select'}>
             <option value={'nova-pochta'}>Новая Почта (почтомат) +50 грн</option>
-            <option id="visible" value={'klad'}>Клад</option>
         </select>
 
         <h3>Способ оплаты</h3>
